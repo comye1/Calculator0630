69e552be3745c50b44ef908bbb9c60c0ea42a2b4
a <a@b71012-PC>
2019-06-30 12:38:41 +0900

    add Test02

---
+++ src/pac1/Test02.java
@@ -0,0 +1,25 @@
+package pac1;
+
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Ã¹ ¹øÂ° ÀÔ·Â °ª: ");
+	    int first = scanner.nextInt();
+	    System.out.println(first);
+
+	    System.out.println("µÎ ¹øÂ° ÀÔ·Â °ª: ");
+	    int second=scanner.nextInt();
+	    System.out.println(second);
+	    
+	    System.out.println("µ¡¼À : "+(first+second));
+		System.out.println("»¬¼À : "+(first-second));
+		System.out.println("°ö¼À : "+(first*second));
+		System.out.println("³ª´°¼À : "+(first/second));
+	    
+		scanner.close();
+	}
+
+}
