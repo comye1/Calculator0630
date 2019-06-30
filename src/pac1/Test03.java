491e5df9b5dba29e216be367ca11a083ae1b0129
a <a@b71012-PC>
2019-06-30 13:01:39 +0900

    연산자 입력받아서 연산하기

---
+++ src/pac1/Test03.java
@@ -0,0 +1,29 @@
+package pac1;
+
+import java.util.Scanner;
+public class Test03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Ã¹¹øÂ° °ª ÀÔ·Â : ");
+		int first=scan.nextInt();
+		System.out.print("µÎ¹øÂ° °ª ÀÔ·Â : ");
+		int second=scan.nextInt();
+		System.out.print("¿¬»êÀÚ ÀÔ·Â : ");
+		String symbol=scan.next();
+		if(symbol.equals("+")) {
+			System.out.println("µ¡¼À : "+(first+second));
+		}else if(symbol.equals("-")) {
+			System.out.println("»¬¼À : "+(first-second));
+		}else if(symbol.equals("*")) {
+			System.out.println("°ö¼À : "+(first*second));
+		}else if(symbol.equals("/")) {
+			System.out.println("³ª´°¼À : "+(first/second));
+		}else {
+			;
+		}
+		scan.close();
+	}
+
+}
