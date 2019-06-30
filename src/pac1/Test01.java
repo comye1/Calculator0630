79e966984e02733b0ee864c697599f22bcac8206
a <a@b71012-PC>
2019-06-30 12:21:21 +0900

    first

---
+++ src/pac1/Test01.java
@@ -0,0 +1,14 @@
+package pac1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("µ¡¼À : "+(3+7));
+		System.out.println("»¬¼À : "+(3-2));
+		System.out.println("°ö¼À : "+(3*4));
+		System.out.println("³ª´°¼À : "+(6/2));
+		
+
+	}
+
+}
