28361584b2cb1b082fdbe29469f3f89754586ca9
a <a@b71012-PC>
2019-06-30 12:46:31 +0900

    두 값 입력받아 사칙연산

--- src/pac1/Test02.java
+++ src/pac1/Test02.java
@@ -6,11 +6,11 @@ public class Test02 {
 
 	public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("첫 번째 입력 값: ");
+	    System.out.println("첫 번째 값: ");
 	    int first = scanner.nextInt();
 	    System.out.println(first);
 
-	    System.out.println("두 번째 입력 값: ");
+	    System.out.println("두 번째 값: ");
 	    int second=scanner.nextInt();
 	    System.out.println(second);
 	    
